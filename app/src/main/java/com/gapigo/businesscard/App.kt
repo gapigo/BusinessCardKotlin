2e30782ac52179b7bf74f9b6ee2e561a36b67b1c
gapigo <x.gabriel12@gmail.com>
2022-08-06 23:28:07 -0300

    Added files for persistent data manipulation of ROOM

--- app/src/main/java/com/gapigo/businesscard/App.kt
+++ app/src/main/java/com/gapigo/businesscard/App.kt
@@ -1,6 +1,10 @@
 package com.gapigo.businesscard
 
 import android.app.Application
+import com.gapigo.businesscard.data.AppDatabase
+import com.gapigo.businesscard.data.BusinessCardRepository
 
 class App : Application() {
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao())}
 }
