0ccb563c926d94798bfd7fdd6b39d6814fa7c41c
gapigo <x.gabriel12@gmail.com>
2022-08-07 00:02:41 -0300

    Added adapter and viewModel for recycler view

--- app/src/main/java/com/gapigo/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/gapigo/businesscard/ui/MainActivity.kt
@@ -3,14 +3,26 @@ package com.gapigo.businesscard.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.gapigo.businesscard.App
 import com.gapigo.businesscard.R
 import com.gapigo.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
+    private val adapter by lazy { BusinessCardAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
         insertListener()
     }
 
@@ -18,6 +30,12 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this) { businessCards ->
+            adapter.submitList(businessCards)
         }
     }
 }
